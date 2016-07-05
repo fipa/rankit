be433b1a9e9cd7bf90bcdf7c025d1b996eb77282
Fernando Ignacio Poblete Arrau <fernando.poblete@lan.com>
2016-07-05 08:45:39 -0400

    Implementing toggles for not-yet used menus (navbar and sidebar)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_features
+
+  def set_features
+  	@sidebar_feature = false
+  end
 end
